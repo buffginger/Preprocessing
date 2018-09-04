b1e3d5c756bded40df3ae7904229a2651317b3cf
Ethan Anderson <andersonethan683@gmail.com>
2018-09-04 13:04:05 -0500

    Finished criteria 2a and 2b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,21 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-
+import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Comparator;
 public class Main {
+
+    // Data structure to hold our words e.g key => value
+    static Map<String, Integer> wordMap = new HashMap<>();
 
     public static void main(String[] args) {
 
-        // Data structure to hold our words e.g key => value
-        Map<String,Integer> wordMap = new HashMap<>();
-        String key = "";
-        Integer value = 0;
+
+        String word = "";
 
         // Set the path to the documentset folder
-        File folder = new File("/Users/ethananderson/Downloads/documentset");
+        File folder = new File("/Users/ethananderson/Downloads/test");
         File[] fileList = folder.listFiles();   // Get the # of total files
 
         // Iterate through each file
@@ -26,6 +29,10 @@ public class Main {
                 // Open the file for reading
                 try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                     String line;    // the line of text from the file
+                    // Skip the first 12 lines of the file, as they aren't needed
+                    for (int i = 0; i < 12; i++) {
+                        br.readLine();
+                    }
                     // Read each line in the file
                     while ((line = br.readLine()) != null) {
 
@@ -37,16 +44,19 @@ public class Main {
 
                         StringTokenizer st = new StringTokenizer(line, " ");
                         while (st.hasMoreElements()) {
-                            // Filter out tags and punctuation with a whitelist function
+                            word = st.nextToken().toLowerCase();
 
-                            // If the token passes, add to wordMap
+                            // Filter out tags
+                            if (isHTMLTag(word) == false) {
+                                word = removePunctuation(word);
+                                wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+                            }
 
-                            // Use variables to check for criteria such as most frequent, etc...
+                            /* a.   How many unique keywords are there in this collection?
+                            b.   What are the top 10 most frequently occurring keywords in the collection?
+                            c.   For each of the top 10 keywords, indicate which ones are meaningful.
+                            d.   What are the bottom 10 keywords in the collection?*/
                         }
-
-
-
-
 
                     }
                 } catch (IOException x) {
@@ -54,6 +64,55 @@ public class Main {
                 }
 
             }
+        } // end for
+
+        displayResults();
+
+        /*for (Map.Entry entry : wordMap.entrySet())
+        {
+            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+        }*/
+
+    }
+
+    public static boolean isHTMLTag(String word) {
+        if (word.matches("<.+?>")) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public static String removePunctuation(String word) {
+        return word.replaceAll("[^\\w\\s]|_", "");
+    }
+
+    public static int numKeyWords() {
+        return wordMap.size();
+    }
+
+    public static void displayResults() {
+        System.out.println("2a) There are " + numKeyWords() + " keywords");
+        sortMap();
+    }
+
+    public static void sortMap() {
+        Object[] sortedMap = wordMap.entrySet().toArray();
+        Arrays.sort(sortedMap, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Integer>) o2).getValue()
+                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+            }
+        });
+        System.out.print("2b) The top ten words are: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(sortedMap[i].toString().replaceFirst("=.*", "") );
+            if (i != 9) {
+                System.out.print(", ");
+            }
+        }
+
+    }
+
+
 }
