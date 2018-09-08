633bd5a5e1b4fa67e2297f8068f54bb818ffaeab
Ethan Anderson <andersonethan683@gmail.com>
2018-09-08 16:51:48 -0500

    Refactored main method to multiple individual methods for simple code re-use. Stop-words are now read into a TreeSet for faster processing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,101 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.Buffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.util.Comparator;
-public class Main {
+import java.util.TreeSet;
+import java.util.Iterator;
 
-    // Data structure to hold our words e.g key => value
-    static Map<String, Integer> wordMap = new HashMap<>();
+public class Main {
 
     public static void main(String[] args) {
 
-
-        String word = "";
+        File stopWords = new File("/Users/ethananderson/Downloads/stop-word-list.txt");
 
         // Set the path to the documentset folder
-        File folder = new File("/Users/ethananderson/Downloads/test");
+        File documentSet = new File("/Users/ethananderson/Downloads/documentset");
+
+        // Create a TreeSet from the provided stopWords file
+        TreeSet<String> stopWordsTree= getStopWordsAsTree(stopWords);
+
+        // Initial wordMap, for #2. 
+        HashMap<String, Integer> wordMap1 = createMapFromDocs(documentSet);
+        displayResults(wordMap1);
+
+    }
+
+    public static boolean isHTMLTag(String word) {
+        if (word.matches("<.+?>")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String removePunctuation(String word) {
+        return word.replaceAll("[^\\w\\s]|_", "");
+    }
+
+    public static int numKeyWords(HashMap<String, Integer> wordMap) {
+        return wordMap.size();
+    }
+
+    public static void displayResults(HashMap<String, Integer> wordMap) {
+        System.out.println("2a) There are " + numKeyWords(wordMap) + " keywords");
+        sortMap(wordMap);
+    }
+
+    public static void sortMap(HashMap<String, Integer> wordMap) {
+        Object[] sortedMap = wordMap.entrySet().toArray();
+        Arrays.sort(sortedMap, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Integer>) o2).getValue()
+                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+            }
+        });
+        System.out.print("2b) The top ten words are: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(sortedMap[i].toString().replaceFirst("=.*", "") );
+            if (i != 9) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("\n2c) The bottom ten words are: ");
+        for (int i = sortedMap.length - 1; i > sortedMap.length -11; i--) {
+            System.out.print(sortedMap[i].toString().replaceFirst("=.*",""));
+            if (i != sortedMap.length -10) {
+                System.out.print(", ");
+            }
+        }
+
+    }
+
+    public static TreeSet<String> getStopWordsAsTree(File stopWords) {
+
+        TreeSet<String> ts = new TreeSet<>();
+
+        try (BufferedReader buff = new BufferedReader(new FileReader(stopWords))) {
+            String entry;
+
+            while ((entry = buff.readLine()) != null) {
+                ts.add(entry.toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ts;
+    }
+
+    public static HashMap<String, Integer> createMapFromDocs(File folder) {
+
+        // Data structure to hold our words e.g key => value
+        HashMap<String, Integer> wordMap = new HashMap<>();
+        String word = "";
         File[] fileList = folder.listFiles();   // Get the # of total files
 
         // Iterate through each file
@@ -64,61 +140,8 @@ public class Main {
 
             }
         } // end for
-
-        displayResults();
-
-        /*for (Map.Entry entry : wordMap.entrySet())
-        {
-            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
-        }*/
+        return wordMap;
 
     }
-
-    public static boolean isHTMLTag(String word) {
-        if (word.matches("<.+?>")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static String removePunctuation(String word) {
-        return word.replaceAll("[^\\w\\s]|_", "");
-    }
-
-    public static int numKeyWords() {
-        return wordMap.size();
-    }
-
-    public static void displayResults() {
-        System.out.println("2a) There are " + numKeyWords() + " keywords");
-        sortMap();
-    }
-
-    public static void sortMap() {
-        Object[] sortedMap = wordMap.entrySet().toArray();
-        Arrays.sort(sortedMap, new Comparator() {
-            public int compare(Object o1, Object o2) {
-                return ((Map.Entry<String, Integer>) o2).getValue()
-                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
-            }
-        });
-        System.out.print("2b) The top ten words are: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(sortedMap[i].toString().replaceFirst("=.*", "") );
-            if (i != 9) {
-                System.out.print(", ");
-            }
-        }
-        System.out.print("\n2c) The bottom ten words are: ");
-        for (int i = sortedMap.length - 1; i > sortedMap.length -11; i--) {
-            System.out.print(sortedMap[i].toString().replaceFirst("=.*",""));
-            if (i != sortedMap.length -10) {
-                System.out.print(", ");
-            }
-        }
-
-    }
-
 
 }
